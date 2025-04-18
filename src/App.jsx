a6cf24c59c89027a02a62b52514bfe342669dc83
abduvokhidoff <abcdefgmananam@gmail.com>
2025-04-18 19:28:42 +0500

    'done'

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Dashboard from './pages/Dashboard'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [loading, setLoading] = useState(true)
+	const [isAuth, setIsAuth] = useState(false)
+	const navigate = useNavigate()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => {
+		const authStatus = localStorage.getItem('isAuth')
+		setIsAuth(authStatus === 'true')
+		setLoading(false)
+	}, [])
+
+	const login = (user) => {
+		localStorage.setItem('isAuth', 'true')
+		localStorage.setItem('user', JSON.stringify(user))
+		setIsAuth(true)
+	}
+
+	const logout = () => {
+		localStorage.setItem('isAuth', 'false')
+		localStorage.removeItem('user')
+		setIsAuth(false)
+		navigate('/login') // Navigate to login page after logout
+	}
+
+	if (loading) {
+		return <div>Loading...</div>
+	}
+
+	return (
+		<>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<Login login={login} />} />
+				<Route
+					path='/dashboard'
+					element={
+						<PrivateRoute isAuth={isAuth}>
+							<Dashboard logout={logout} />
+						</PrivateRoute>
+					}
+				/>
+			</Routes>
+		</>
+	)
 }
 
 export default App
