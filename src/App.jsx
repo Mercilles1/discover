52c69dfb1dfb218c07ec2a7ce281883dee8d653c
Mersillec1 <rahmatillaqobilov90@gmail.com>
2025-04-18 19:51:27 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomePage from './HomePage'
 import { useEffect, useState } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import Home from './pages/Home'
@@ -13,19 +10,6 @@ function App() {
 	const [isAuth, setIsAuth] = useState(false)
 	const navigate = useNavigate()
 
-  return (
-    <Router>
-      <div className='flex justify-center'>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/search" element={<div className='w-[390px] px-[24px] pt-[12px]'><h1 className='text-2xl font-bold'>Search Page</h1></div>} />
-          <Route path="/saved" element={<div className='w-[390px] px-[24px] pt-[12px]'><h1 className='text-2xl font-bold'>Saved Items</h1></div>} />
-          <Route path="/cart" element={<div className='w-[390px] px-[24px] pt-[12px]'><h1 className='text-2xl font-bold'>Shopping Cart</h1></div>} />
-          <Route path="/account" element={<div className='w-[390px] px-[24px] pt-[12px]'><h1 className='text-2xl font-bold'>Account Page</h1></div>} />
-        </Routes>
-      </div>
-    </Router>
-  )
 	useEffect(() => {
 		const authStatus = localStorage.getItem('isAuth')
 		setIsAuth(authStatus === 'true')
