d729020cb456327879420d9c68e62f245d68891b
muhamejanov11 <muhamejanov.n@gmail.com>
2025-04-18 19:41:42 +0500

    done

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -60,7 +60,7 @@ export default function Cart() {
 
     return (
         <div className="flex flex-col h-screen bg-white w-[390px] mx-auto">
-            <div className="px-4 py-4 border-b text-center text-lg font-semibold flex items-center">
+            <div className="px-4 py-4 border-b text-center text-lg font-semibold flex items-center justify-between">
                 <img onClick={() => navigate("/")} src={arrow} alt="" />
                 <p>My cart</p>
             </div>
