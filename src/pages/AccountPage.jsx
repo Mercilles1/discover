3e6c0f944bb3cbf3787cf3351fe515c0e15efbd0
abduvokhidoff <abcdefgmananam@gmail.com>
2025-04-23 20:04:53 +0500

    'done'

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AccountPage = () => {
+  const [user, setUser] = useState()
+
+  const CurrentUser = localStorage.getItem('user')
+  fetch('')
   return (
-    <div>AccountPage</div>
+    <div>
+      
+
+    </div>
   )
 }
 
